dda8463d3855a3ebe22d430d9baef107c7ee26cc
gagangani13 <gagangani17@gmail.com>
2023-06-04 12:03:17 +0530

    Encrypting password

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,35 +1,49 @@
 const {User}=require('../model/user')
+const bcrypt=require('bcrypt')
 module.exports.newUser=async(req,res,next)=>{
+    if(!req.body.email || !req.body.password){
+        return res.status(400).json({err:'Invalid Input'})
+    }
     try {
         const email=req.body.email
         const password=req.body.password
-        if(!email || !password){
-            return res.status(400).json({err:'Invalid Input'})
-        }
-        const createUser=await User.create({
-            email:email,
-            password:password
+        bcrypt.hash(password,10,async(err,result)=>{
+            if (err){
+                throw new Error()
+            }
+            const createUser=await User.create({
+                email:email,
+                password:result
+            })
+            console.log(createUser);
+            res.status(201).json({message:"User Added"})
         })
-        console.log(createUser);
-        res.status(201).json({message:"User Added"})
     } catch (error) {
         res.status(500).send({message:"User Exists"})
     }
 }
 
 module.exports.loginUser=async(req,res,next)=>{
+    const email=req.body.email
+    const password=req.body.password
+    if(!email || !password){
+        return res.status(400).send({message:'Invalid Input',ok:false})
+    }
     try {
-        const email=req.body.email
-        const password=req.body.password
-        if(!email || !password){
-            return res.status(400).send({message:'Invalid Input',ok:false})
-        }
         const getUser=await User.findAll({where:{email:email}})
-        if (getUser[0].password===password){
-            res.status(200).send({message:'User Logged in',ok:true})
-        }else{
-            throw new Error('password incorrect')
+        if(!getUser){
+            throw new Error("User doesn't exists")
         }
+        bcrypt.compare(password,getUser[0].password,(err,result)=>{
+            if (err) {
+                throw new Error('Something went wrong')
+            }
+            else if (result) {
+                res.status(200).send({message:'User Logged in',ok:true})          
+            } else {
+                return res.status(404).send("Incorrect password")
+            }
+        })
     } catch (error) {
         res.status(404).send({message:error.message,ok:false})
     }
