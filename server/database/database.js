e403a8aa70215a2284c56e64671552fa8afb82d9
gagangani13 <gagangani17@gmail.com>
2023-06-12 11:33:53 +0530

    Backend deployment

--- server/database/database.js
+++ server/database/database.js
@@ -2,6 +2,6 @@ const {Sequelize}=require('sequelize')
 require('dotenv').config()
 const database=new Sequelize(process.env.DATABASE_NAME,process.env.DATABASE_USER,process.env.DATABASE_PASSWORD,{
     dialect:'mysql',
-    host:'localhost'
+    host:process.env.DATABASE_HOST
 })
 module.exports=database
